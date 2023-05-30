e7baf32e7f49646b9011e457f80cdae3bef4d4ed
Floyd Zweydinger <floyd.zweydinger+git@rub.de>
2023-05-30 16:12:14 +0200

    updated nix.shell

--- shell.nix
+++ shell.nix
@@ -1,22 +1,37 @@
-{ pkgs ? import <nixpkgs> {} }:
+with (import <nixpkgs> {});
 let
-  my-python = pkgs.python38;
-  python-with-my-packages = my-python.withPackages (p: with p; [
-    #pygments
-	antlr4-python3-runtime
-	Opcodes
-	pycca
-	#pytest
-	#pytest-benchmark
-  ]);
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix";
+    ref = "refs/tags/3.5.0";
+  }) {};
+  pyEnv = mach-nix.mkPython rec {
+    providers._default = "wheel,conda,nixpkgs,sdist";
+    requirements = builtins.readFile ./requirements.txt;
+  };
 in
-pkgs.mkShell {
+
+mach-nix.nixpkgs.mkShell {
   buildInputs = [
-    python-with-my-packages
-	pkgs.python-language-server
-	antlr4_9
+    wget
+    virtualenv
+	antlr4_8
+    pyEnv
+	python3Packages.antlr4-python3-runtime
   ];
+  venvDir = "venv3";
+
   shellHook = ''
-    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+    virtualenv --no-setuptools venv
+    export PIP_PREFIX=$(pwd)/_build/pip_packages
+    export PATH=$PWD/venv/bin:$PATH
+    export PYTHONPATH=venv/lib/python3/site-packages/:$PYTHONPATH
+    export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    unset SOURCE_DATE_EPOCH
+    sh setup.sh
+  '';
+
+   postShellHook = ''
+    ln -sf ${pyEnv}/lib/python3/site-packages/* ./venv/lib/python3/site-packages
   '';
 }
--- src/x64Visitor.py
+++ src/x64Visitor.py
@@ -1,4 +1,4 @@
-# Generated from x64.g4 by ANTLR 4.11.1
+# Generated from x64.g4 by ANTLR 4.8
 from antlr4 import *
 if __name__ is not None and "." in __name__:
     from .x64Parser import x64Parser
